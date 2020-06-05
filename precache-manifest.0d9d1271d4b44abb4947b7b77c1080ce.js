a8e86375110ad5eb23dff21bd9e4b0cfac3583e5
Connor Kanalec <57271684+ConkyCode@users.noreply.github.com>
2020-06-05 13:43:59 -0400

    Updates

---
+++ precache-manifest.0d9d1271d4b44abb4947b7b77c1080ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ccacc596ee48a104c9c293291c40174",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "be01aae1ea341542f8b7",
+    "url": "/robofriends/static/css/main.fdbbf3cb.chunk.css"
+  },
+  {
+    "revision": "8ac66cf85c535b6f0889",
+    "url": "/robofriends/static/js/2.a8a83e8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a8a83e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be01aae1ea341542f8b7",
+    "url": "/robofriends/static/js/main.5f3ee7dc.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
